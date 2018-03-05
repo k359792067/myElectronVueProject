65fce6798368cf8748ac4579089821e991348d47
k359792067 <359792067@qq.com>
2018-03-05 16:28:59 +0800

    多个swiper联动
    多个swiper联动

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -16,4 +16,15 @@
     background-color: #f0f3f6;
     overflow: hidden;
   }
+
+  html,
+  body {
+    margin: 0 auto;
+    padding: 0;
+    height: 100%;
+    text-align: center;
+    background-color: #f0f3f6;
+    overflow: hidden;
+    /* -webkit-user-select: none; */
+  }
 </style>
--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -1,20 +1,60 @@
 <template>
   <div class="index-bg">
-    <div class="swiper-navbar">
-      <swiper :options="swiperOption">
+    <div class="swiper-content">
+      <swiper :options="swiperOptionContent" ref="swiperContent">
         <!-- 视差效果 -->
         <div class="parallax-bg" slot="parallax-bg" data-swiper-parallax="-23%"></div>
         <!-- <swiper-slide v-for="slide in swiperSlides" :key="slide">Slide {{ slide }}</swiper-slide> -->
-        <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-        <swiper-slide>Slide 4</swiper-slide>
-        <swiper-slide>Slide 5</swiper-slide>
-        <swiper-slide>Slide 6</swiper-slide>
-        <swiper-slide>Slide 7</swiper-slide>
-        <swiper-slide>Slide 8</swiper-slide>
-        <swiper-slide>Slide 9</swiper-slide>
-        <swiper-slide>Slide 10</swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/building.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/car.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/flag.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/gonggong.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/honor.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/news.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/user.png" />
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+    <div class="swiper-navbar">
+      <swiper :options="swiperOptionNavbar" ref="swiperNavbar">
+        <!-- 视差效果 -->
+        <div class="parallax-bg" slot="parallax-bg" data-swiper-parallax="-23%"></div>
+        <!-- <swiper-slide v-for="slide in swiperSlides" :key="slide">Slide {{ slide }}</swiper-slide> -->
+        <swiper-slide>
+          <img src="../assets/img/index/building.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/car.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/flag.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/gonggong.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/honor.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/news.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="../assets/img/index/user.png" />
+        </swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
       </swiper>
     </div>
@@ -30,13 +70,20 @@
   export default {
     data () {
       return {
-        swiperOption: {
+        swiperOptionContent: {
           slidesPerView: 3,
+          loopedSlides: 7,
           spaceBetween: 30,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-          }
+          loop: true,
+          centeredSlides: true
+        },
+        swiperOptionNavbar: {
+          slidesPerView: 7,
+          loopedSlides: 7,
+          spaceBetween: 15,
+          loop: true,
+          centeredSlides: true,
+          slideToClickedSlide: true
         }
         // swiperSlides: [1, 2, 3, 4, 5, 6, 7]
       }
@@ -44,18 +91,28 @@
     components: {
       swiper,
       swiperSlide
+    },
+    mounted () {
+      this.$nextTick(() => {
+        const swiperContent = this.$refs.swiperContent.swiper
+        console.log(swiperContent)
+        const swiperNavbar = this.$refs.swiperNavbar.swiper
+        console.log(swiperNavbar)
+        swiperContent.controller.control = swiperNavbar
+        swiperNavbar.controller.control = swiperContent
+      })
     }
   }
 </script>
 
 <style scoped>
-  .login-bg {
-    background: url('../assets/img/index-bg.jpg') no-repeat;
+  .index-bg {
+    background: url('../assets/img/index/index-bg.png') no-repeat;
     width: 100%;
     height: 100%;
     text-align: center;
   }
-  
+
   .parallax-bg {
     position: absolute;
     left: 0;
@@ -63,8 +120,10 @@
     width: 130%;
     height: 100%;
     -webkit-background-size: cover;
+    background-color: #82eaf1;
+    opacity: 0.2;
     background-size: cover;
     background-position: center;
-    background-image: url(/static/images/surmon-1.jpg);
+    /* background-image: url(/static/images/surmon-1.jpg); */
   }
 </style>
